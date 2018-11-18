0a8f21efd89aae2f17cf34132a1ca5d54d572b3c
Luca Schroeder <lucaschroeder@Lucas-MacBook-Pro.local>
2018-11-18 09:16:10 -0800

    drop future knowledge cols

--- heartbreaker/data_loader.py
+++ heartbreaker/data_loader.py
@@ -104,6 +104,18 @@ def load_usda_food_env_table(fname):
     df.drop(columns=df.select_dtypes(exclude='number'), inplace=True)
     if "FIPS" in df.columns:  # Needs special handling because this will appear numeric
         df.drop(columns='FIPS', inplace=True)
+        
+    # Drop any data that is observed AFTER our heart disease data (2014)
+    future_knowledge_cols = []
+    for column in df.columns:
+        try:
+            year = int(column[-2:])
+            if (year > 14):
+                future_knowledge_cols.append(column)
+        except:
+            continue
+
+    df.drop(columns=future_knowledge_cols, inplace=True)
 
     return df
 
