bc85da7e8def570d1d7a72d7d0722885f95f94f0
Kevin Wu <crazyviolindude@gmail.com>
2018-12-03 18:34:52 -0800

    Initial code for backwards feature search

---
+++ heartbreaker/interpretation.py
@@ -0,0 +1,69 @@
+"""
+Code for doing ML model interpretation
+"""
+
+import os
+import sys
+import logging
+import multiprocessing
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import sklearn.metrics
+
+import data_loader
+import util
+
+def fit_and_predict(model, x_train, y_train, x_test):
+    """Take a model that has .fit and .predict methods, train it, and return its predictions"""
+    model.fit(x_train, y_train)
+    return model.predict(x_test)
+
+def feature_backwards_search(x_train, y_train, x_test, y_test, model, **kwargs):
+    """
+    Do a depth-of-one feature backwards search. Does not assume that the input is standardized.
+    kwargs are paseed to the model initialization.
+    """
+    # Standardize
+    sc = StandardScaler()
+    x_train_std = sc.fit_transform(x_train)
+    x_test_std = sc.transform(x_test)
+    assert x_train_std.shape[1] == x_test_std.shape[1]
+    
+    # Create subsetted input data sets
+    x_train_std_dropped = [np.delete(x_train_std, i, 1) for i in range(x_train_std.shape[1])]
+    x_test_std_dropped = [np.delete(x_test_std, i, 1) for i in range(x_test_std.shape[1])]
+    for training_set in x_train_std_dropped:  # sanity check
+        assert training_set.shape == (training_set.shape[0], x_train_std.shape[1] - 1)
+    
+    # Create a bunch of models, one for each dropped dataset, using kwargs
+    models = [model(**kwargs) for _i in range(x_train_std.shape[1])]
+    
+    # Fit each in parallel
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    predictions = pool.starmap(fit_and_predict, [(model, train_data, y_train, test_data) for model, train_data, test_data in zip(models, x_train_std_dropped, x_test_std_dropped)])
+    pool.close()
+    pool.join()
+
+    # Evaluate them
+    baseline_predictions = fit_and_predict(model(**kwargs), x_train_std, y_train, x_test_std)
+    baseline_score = sklearn.metrics.f1_score(y_test, baseline_predictions)
+    logging.info("Baseline score with full data: {}".format(baseline_score))
+    dropped_scores = [sklearn.metrics.f1_score(y_test, preds) for preds in predictions]
+    logging.info("Min and max scores after dropping features: {} {}".format(min(dropped_scores), max(dropped_scores)))
+
+def main(percentile=25):
+    """Execute script"""
+    data = util.impute_by_col(data_loader.load_all_data())
+    rates = data.pop('heart_disease_mortality')
+    rates_high_low = util.continuous_to_categorical(rates, 100 - percentile)
+    train_validation_partitions, test_set = util.split_train_valid_k_fold(data, rates_high_low)
+
+    # Use the first split
+    x_train, y_train, x_test, y_test = train_validation_partitions[0]
+    feature_backwards_search(x_train, y_train, x_test, y_test, LogisticRegression, solver='liblinear', class_weight='balanced')
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
