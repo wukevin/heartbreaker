5adb6c4540e3d99e780accf1e569d657ea0ca7bd
Kevin Wu <crazyviolindude@gmail.com>
2018-12-03 21:07:32 -0800

    Minor tweaks

--- heartbreaker/interpretation.py
+++ heartbreaker/interpretation.py
@@ -35,6 +35,7 @@ def feature_backwards_search(x_train, y_train, x_test, y_test, fname, model, **k
     assert x_train_std.shape[1] == x_test_std.shape[1]
     
     # Create subsetted input data sets
+    logging.info("Evaluating {} features".format(x_train_std.shape[1]))
     x_train_std_dropped = [np.delete(x_train_std, i, 1) for i in range(x_train_std.shape[1])]
     x_test_std_dropped = [np.delete(x_test_std, i, 1) for i in range(x_test_std.shape[1])]
     for training_set in x_train_std_dropped:  # sanity check
@@ -44,7 +45,7 @@ def feature_backwards_search(x_train, y_train, x_test, y_test, fname, model, **k
     models = [model(**kwargs) for _i in range(x_train_std.shape[1])]
     
     # Fit each in parallel
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    pool = multiprocessing.Pool(int(multiprocessing.cpu_count()))
     predictions = pool.starmap(fit_and_predict, [(model, train_data, y_train, test_data) for model, train_data, test_data in zip(models, x_train_std_dropped, x_test_std_dropped)])
     pool.close()
     pool.join()
@@ -78,7 +79,8 @@ def main(percentile=25):
 
     # Use the first split
     x_train, y_train, x_test, y_test = train_validation_partitions[0]
-    feature_backwards_search(x_train, y_train, x_test, y_test, "", LogisticRegression, solver='liblinear', class_weight='balanced')
+    feature_backwards_search(x_train, y_train, x_test, y_test, "", LogisticRegression,
+        solver='liblinear', class_weight='balanced', C=1, penalty='l1', random_state=98572, max_iter=1000)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
