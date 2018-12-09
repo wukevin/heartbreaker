d628a88f919c4a30bb5e718f8b4d195306b5c7e8
Kevin Wu <crazyviolindude@gmail.com>
2018-12-09 12:07:38 -0800

    Add ability to disable log reg feature selection in pipeline
    Disable this for Xgboost parameter search

--- heartbreaker/model_boosted_tree.py
+++ heartbreaker/model_boosted_tree.py
@@ -87,11 +87,11 @@ def parameter_sweep_pipeline(percentile=25, depth_candidates=[4, 6, 8], num_est_
         "n_estimators": num_est_candidates,
         "reg_alpha": [0.001, 0.01, 0.1, 1, 10, 100]
     }
-    models.append((boosted_tree, boosted_tree_params, False))
+    models.append((boosted_tree, boosted_tree_params))
 
     for (model, params, scale) in models:
         grid_params = adjust_params(params)
-        cv = get_gscv(model, grid_params, scale=scale)
+        cv = get_gscv(model, grid_params, scale=False, preselect_features=False)
         cv.fit(x_train, y_train)
 
         logging.info(model.__class__.__name__)
