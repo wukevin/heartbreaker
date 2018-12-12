2a55418bb0b813abb5df1a9399ce4a6e7964fd81
Luca Schroeder <LucaSchroeder@github.com>
2018-12-11 20:35:25 -0800

    add visualization and modify test code

--- heartbreaker/classification.py
+++ heartbreaker/classification.py
@@ -21,6 +21,7 @@ from sklearn.svm import SVC
 import data_loader
 import util
 import plotting
+from sklearn.utils import class_weight
 
 import xgboost
 
@@ -40,17 +41,23 @@ from sklearn.feature_selection import SelectFromModel
 
 seed = 754927
 
-def classification(model, x_train, y_train, seed=seed):   
+def classification_train(model, x_train, y_train, scale=True, preselect_features=False, seed=seed):   
     scoring = {
             'acc': make_scorer(accuracy_score),
             'prec': make_scorer(precision_score),
             'recall': make_scorer(recall_score),
             'f1': make_scorer(f1_score)
             }
-    pipeline = Pipeline([
-            ('transformer', StandardScaler()), 
-            ('feature_selection', SelectFromModel(LogisticRegression(penalty='l1', max_iter=1000, solver='liblinear', random_state=seed))), 
-            ('estimator', model)])
+    
+    pipeline_jobs = []
+    if (scale):
+        pipeline_jobs.append(('transformer', StandardScaler()))
+    if (preselect_features):
+        pipeline_jobs.append(('feature_selection', SelectFromModel(LogisticRegression(penalty='l1', max_iter=1000, solver='liblinear', random_state=seed))))
+    
+    pipeline_jobs.append(('estimator', model))
+    
+    pipeline = Pipeline(pipeline_jobs)
         
     kf = KFold(n_splits=10, random_state=seed)
     # This is a dictionary of outputs
@@ -64,13 +71,18 @@ def classification(model, x_train, y_train, seed=seed):
     logging.info(model.__class__.__name__)
     logging.info("Accuracy: %0.4f, Precision: %0.4f, Recall: %0.4f, F1: %0.4f" % (accuracy, precision, recall, f1))
 
-    return cv_results['estimator']
+    return (accuracy, precision, recall, f1)
 
-def classification2(model, x_train, y_train, x_test, y_test, seed=seed):   
-    pipeline = Pipeline([
-            ('transformer', StandardScaler()), 
-            ('feature_selection', SelectFromModel(LogisticRegression(penalty='l1', max_iter=1000, solver='liblinear', random_state=seed))), 
-            ('estimator', model)])
+def classification_test(model, x_train, y_train, x_test, y_test, scale=True, preselect_features=False, seed=seed):   
+    pipeline_jobs = []
+    if (scale):
+        pipeline_jobs.append(('transformer', StandardScaler()))
+    if (preselect_features):
+        pipeline_jobs.append(('feature_selection', SelectFromModel(LogisticRegression(penalty='l1', max_iter=1000, solver='liblinear', random_state=seed))))
+    
+    pipeline_jobs.append(('estimator', model))
+    
+    pipeline = Pipeline(pipeline_jobs)
 
     pipeline.fit(x_train, y_train)
     
@@ -84,7 +96,7 @@ def classification2(model, x_train, y_train, x_test, y_test, seed=seed):
     logging.info(model.__class__.__name__)
     logging.info("Accuracy: %0.4f, Precision: %0.4f, Recall: %0.4f, F1: %0.4f" % (accuracy, precision, recall, f1))
 
-    return pipeline
+    return (accuracy, precision, recall, f1)
 
 def main():
     data = util.impute_by_col(data_loader.load_all_data(), np.mean)
@@ -100,29 +112,43 @@ def main():
     logistic = LogisticRegression(penalty='l1', C=0.1, max_iter=1000, solver='liblinear', class_weight='balanced')
     models.append(logistic)
     
-    dt = DecisionTreeClassifier(criterion="gini", max_depth=None, max_features=None, min_samples_leaf=0.01, class_weight='balanced') #gini loss
-    models.append(dt)
-    
-    rf = RandomForestClassifier(criterion="entropy", max_depth=None, max_features=None, min_samples_leaf=0.001, n_estimators=1000, class_weight='balanced')
-    models.append(rf)
-    
-    svc_rbf = SVC(kernel='rbf', C=1, random_state=seed, class_weight='balanced')
     svc_linear = SVC(kernel='linear', C=0.01, random_state=seed, class_weight='balanced')
+    svc_rbf = SVC(kernel='rbf', C=1.0, random_state=seed, class_weight='balanced')
     svc_sigmoid = SVC(kernel='sigmoid', C=0.1, random_state=seed, class_weight='balanced')
-    models.append(svc_rbf)
     models.append(svc_linear)
+    models.append(svc_rbf)
     models.append(svc_sigmoid)
     
-    models = []
-    xgb = xgboost.XGBClassifier(learning_rate=1e-2, max_depth=6, n_estimators=350, random_state=seed, class_weights='balanced')
+    dt = DecisionTreeClassifier(criterion="entropy", max_depth=None, max_features=None, min_samples_leaf=0.01, class_weight='balanced', random_state=seed) #gini loss
+    models.append(dt)
+    
+    rf = RandomForestClassifier(criterion="entropy", max_depth=None, max_features='sqrt', min_samples_leaf=0.01, n_estimators=100, class_weight='balanced', random_state=seed)
+    models.append(rf)
+    
+    weights = class_weight.compute_class_weight('balanced', [0, 1], y_train)
+    weight_ratio = weights[1] / weights[0]
+    xgb = xgboost.XGBClassifier(max_depth=8, learning_rate=1e-2, n_estimators=350, reg_alpha=1, reg_lambda=0, scale_pos_weight=weight_ratio, random_state=seed)
     models.append(xgb)
     
-    trained_models = []
+    tex = "\\documentclass[]{article}\n\\begin{document}\n"
+    tex += "\\begin{tabular}{l||c|c|c|c||c|c|c|c||}\n"
+    tex += "& \\multicolumn{4}{c||}{Results on Training Set} & \\multicolumn{4}{c||}{Results on Test Set}\\\\ \n"
+    tex += "Model & Accuracy & Precision & Recall & $F_1$ score & Accuracy & Precision & Recall & $F_1$ score  \\\\ \\hline\n"
     for model in models:
-        trained_models.append(classification(model, x_train, y_train, seed))
-        trained_models.append(classification2(model, x_train, y_train, x_test, y_test))
+        train_accuracy, train_precision, train_recall, train_f1 = classification_train(model, x_train, y_train, seed)
+        test_accuracy, test_precision, test_recall, test_f1 = classification_test(model, x_train, y_train, x_test, y_test)
+        
+        model_name = model.__class__.__name__
+        if (model_name == "SVC"):
+            model_name += (" (`%s' kernel)" % model.kernel)
+        
+        tex += "%s & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f & %0.4f \\\\ \\hline\n" % (
+                model_name, train_accuracy, train_precision, train_recall, train_f1, 
+                test_accuracy, test_precision, test_recall, test_f1)
     
-    # plotting.plot_shap_tree_summary(trained_models[-1][1], x_train, x_test)
+    tex += "\\end{tabular}\n\\end{document}"
+    
+    print(tex)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
